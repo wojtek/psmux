b578300173f26777dd4c847160c08e63b9d0b60e
Godwin <uniquegodwin@gmail.com>
2025-12-01 21:27:51 +0530

    Update Chocolatey package for v0.2.0

--- packages/chocolatey/tools/chocolateyinstall.ps1
+++ packages/chocolatey/tools/chocolateyinstall.ps1
@@ -1,22 +1,22 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/marlocarlo/psmux/releases/download/v0.1.0/psmux-windows-x86_64.zip'
+$url64 = 'https://github.com/marlocarlo/psmux/releases/download/v0.2.0/psmux-0.2.0-windows-x64.zip'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
-  checksum64     = 'f3305fb9cd4ba44e516898de0b18a06b8b32e90b21f2047f05a9123eaa37ac79'
+  checksum64     = '1496F7834A1DE10D946E8433C16AB0F0C7D60C6C6DBE149119D7B85115B4CA27'
   checksumType64 = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
 # Create shims for psmux, pmux, and tmux
-$psmuxPath = Join-Path $toolsDir "psmux-windows-x86_64\psmux.exe"
-$pmuxPath = Join-Path $toolsDir "psmux-windows-x86_64\pmux.exe"
-$tmuxPath = Join-Path $toolsDir "psmux-windows-x86_64\tmux.exe"
+$psmuxPath = Join-Path $toolsDir "psmux.exe"
+$pmuxPath = Join-Path $toolsDir "pmux.exe"
+$tmuxPath = Join-Path $toolsDir "tmux.exe"
 
 Install-BinFile -Name "psmux" -Path $psmuxPath
 Install-BinFile -Name "pmux" -Path $pmuxPath
