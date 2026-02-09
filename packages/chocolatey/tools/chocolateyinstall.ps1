c2d2a3ca01374cb1e8c73cd318deceb596956ed5
computersrmyfriends <computersrmyfriends@users.noreply.github.com>
2026-02-09 17:09:02 +0530

    chore: update Chocolatey package to v0.2.7

--- packages/chocolatey/tools/chocolateyinstall.ps1
+++ packages/chocolatey/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://github.com/marlocarlo/psmux/releases/download/v0.2.2/psmux-v0.2.2-windows-x64.zip'
+$url64 = 'https://github.com/marlocarlo/psmux/releases/download/v0.2.7/psmux-0.2.7-windows-x64.zip'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   url64bit       = $url64
-  checksum64     = 'DA87D54E374493A6CF8F8BE760941A3123EF8024EF89C797671A89D6B58EAFDF'
+  checksum64     = '18D1BA8E34E2A72FC39D9713E9038C50F3B9AEB0BC9AFB56096E08C40CD92655'
   checksumType64 = 'sha256'
 }
 
